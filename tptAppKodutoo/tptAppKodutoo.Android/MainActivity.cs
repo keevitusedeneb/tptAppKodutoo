d2fbe7b475fb879b3ca496e7bdf857abf7a41943
keevitusedeneb <deos@hot.ee>
2018-05-22 17:55:20 +0300

    appcenternuget

--- tptAppKodutoo/tptAppKodutoo.Android/MainActivity.cs
+++ tptAppKodutoo/tptAppKodutoo.Android/MainActivity.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
@@ -14,6 +16,10 @@ namespace tptAppKodutoo.Droid
     {
         protected override void OnCreate(Bundle bundle)
         {
+
+            AppCenter.Start("50656c02-4185-4719-af89-2d1cdc2f43a1",
+                   typeof(Analytics), typeof(Crashes));
+            AppCenter.Start("50656c02-4185-4719-af89-2d1cdc2f43a1", typeof(Analytics), typeof(Crashes));
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
